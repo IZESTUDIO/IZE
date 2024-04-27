5fbb44ea30853bb6ac8fb85e5848df339a4dd381
IZE STUDIO <166324173+IZESTUDIO@users.noreply.github.com>
2024-04-27 20:10:21 +0700

    new

--- Learn Python/Main.py
+++ Learn Python/Main.py
@@ -1,21 +1,3 @@
-# age = int(input("ป้อนอายุของคุณ: "))
-
-# if age>=1:
-#     print("คำนำหน้าเป็น เด็กชาย/เด็กหญิง")
-# elif age>=0:
-#     print("ไม่สามารถระบุได้")
-#     if age>=15:
-#         print("คำนำหน้าเป็น นาย/นางสาว")
-
-
-# num = int(input("ป้อนตัวเลขที่ต้องการเเสดงตารางสูตรคูณ: "))
-# print("ตารางสูตรคูณของเลข {} ได้เเก่".format(num))
-# for i in range(1,13):
-#     print("{}x{}={}".format(num,i,num*i))
-
-def calculate_length(text):
-    return len(text)
-input_number = input("Insert your text: ")
-length = calculate_length(input_number)
-print("The number of your text is total.",length)
-
+name = 'วรากร รัตนโสภา'
+print(name)
+print(type(name)==str)
